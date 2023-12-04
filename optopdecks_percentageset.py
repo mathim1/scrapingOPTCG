3264bf2a49019ca55833d0c6c4ff08d7c839c15a
Mathias Merino Carvajal <mathias.merinocarvajal@gmail.com>
2023-12-04 04:57:30 -0300

    optopdeckspercetangeupdate

--- optopdecks_percentageset.py
+++ optopdecks_percentageset.py
@@ -4,22 +4,25 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from collections import Counter
 from urllib.parse import unquote
-import time
 import threading
 import re
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-def extraer_cartas_desde_url_selenium(url, semaphore):
+def extraer_cartas_desde_url_selenium(url, semaphore, chrome_driver_path):
     with semaphore:
-        service = Service(ChromeDriverManager().install())
+        service = Service(chrome_driver_path)
         driver = webdriver.Chrome(service=service)
 
         try:
             url_decodificada = unquote(url)
             driver.get(url_decodificada)
-            time.sleep(15)  # Esperar a que se cargue el contenido dinámico
 
-            textarea = driver.find_element(By.TAG_NAME, 'textarea')
+            # Esperar a que el elemento esté presente
+            wait = WebDriverWait(driver, 30)  # Espera hasta 30 segundos
+            textarea = wait.until(EC.presence_of_element_located((By.TAG_NAME, 'textarea')))
+
             texto_decodificado = unquote(textarea.text)
             cartas = eval(texto_decodificado)
             return cartas
@@ -30,8 +33,8 @@ def extraer_cartas_desde_url_selenium(url, semaphore):
             driver.quit()
 
 
-def extraer_cartas_en_thread(url, resultados, semaphore):
-    cartas = extraer_cartas_desde_url_selenium(url, semaphore)
+def extraer_cartas_en_thread(url, resultados, semaphore, chrome_driver_path):
+    cartas = extraer_cartas_desde_url_selenium(url, semaphore, chrome_driver_path)
     resultados[url] = cartas
 
 
@@ -58,14 +61,16 @@ def obtener_urls_de_usuario():
     print("Por favor, ingresa las URLs separadas por una nueva línea. Escribe 'fin' para terminar.")
     urls = []
     while True:
-        url = input()
+        url = input().strip()  # Eliminar espacios en blanco al principio y al final
         if url.lower() == 'fin':
             break
-        urls.append(url)
+        if url:  # Añadir la URL solo si no está vacía
+            urls.append(url)
     return urls
 
 
 def main():
+    chrome_driver_path = ChromeDriverManager().install()
     urls = obtener_urls_de_usuario()  # Obtener URLs del usuario
 
     resultados = {}
@@ -74,7 +79,7 @@ def main():
 
     # Crear y comenzar los hilos
     for url in urls:
-        hilo = threading.Thread(target=extraer_cartas_en_thread, args=(url, resultados, semaphore))
+        hilo = threading.Thread(target=extraer_cartas_en_thread, args=(url, resultados, semaphore, chrome_driver_path))
         hilos.append(hilo)
         hilo.start()
 
